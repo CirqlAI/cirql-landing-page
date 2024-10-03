ae351a6dcd12c21d30b03923f4748b035c354461
Vivek Puttaparthi <vivek@addies.com>
2024-10-03 14:19:01 -0700

    Update testimonial

--- components/Home/Testimonials.tsx
+++ components/Home/Testimonials.tsx
@@ -6,24 +6,27 @@ const Testimonials = () => {
     <section className="bg-primary-light  ">
       <div className="py-10 md:py-24 max-w-[1440px] mx-auto px-5 sm:px-10 lg:px-16">
         <div className="flex items-center flex-col justify-center text-center">
-          <h2 className="text-dark text-4xl sm:text-5xl font-medium">
-            I took one meeting and it paid for the entire year!
+          <h2 className="text-dark text-4xl sm:text-5xl font-medium mb-6">
+            Best concept I have seen in sales in a long time!
           </h2>
-
-          <div className="flex items-center justify-center flex-col mt-10">
-            <Image
-              src="/images/avatar.png"
-              alt="avatar"
-              width={64}
-              height={64}
-              priority
-              quality={100}
-            />
-            <h4 className=" mt-4 font-semibold text-dark text-lg">
-              Maisie Tucker
-            </h4>
-            <p className="text-base ">Marketing Director, US Company</p>
-          </div>
+          <img src="/images/testimonial.png" alt="testimonial"/>
+          {/*<h2 className="text-dark text-4xl sm:text-5xl font-medium">*/}
+          {/*  I took one meeting and it paid for the entire year!*/}
+          {/*</h2>*/}
+          {/*<div className="flex items-center justify-center flex-col mt-10">*/}
+          {/*  <Image*/}
+          {/*      src="/images/avatar.png"*/}
+          {/*      alt="avatar"*/}
+          {/*      width={64}*/}
+          {/*      height={64}*/}
+          {/*      priority*/}
+          {/*      quality={100}*/}
+          {/*  />*/}
+          {/*  <h4 className=" mt-4 font-semibold text-dark text-lg">*/}
+          {/*    Maisie Tucker*/}
+          {/*  </h4>*/}
+          {/*  <p className="text-base ">Marketing Director, US Company</p>*/}
+          {/*</div>*/}
         </div>
       </div>
     </section>
